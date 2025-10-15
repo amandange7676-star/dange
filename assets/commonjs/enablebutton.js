5bad6a0751511e8d8d87ba45bbc93d9b5a303d4a
amandange7676-star <amandange7676@gmail.com>
2025-10-15 17:53:43 +0530

    Update enablebutton.js

--- assets/commonjs/enablebutton.js
+++ assets/commonjs/enablebutton.js
@@ -1,8 +1,9 @@
 const toggle = document.getElementById('myToggle');
-const projectId = window.location.pathname.split('/').pop();  // Assuming the project ID is part of the URL path, adjust accordingly
+let projectId = window.location.pathname.split('/').pop();  // Assuming the project ID is part of the URL path, adjust accordingly
 
 // Fetch the initial feature status from the backend when the page loads
-fetch(`/get_status/?project_url=${projectId}`)
+projectId ="https://amandange7676-star.github.io/meep/"
+fetch(`http://127.0.0.1:8000/get_status/?project_url=${projectId}`)
   .then(response => response.json())
   .then(data => {
     const featureEnabled = data.featureEnabled;
@@ -14,13 +15,14 @@ fetch(`/get_status/?project_url=${projectId}`)
     // Listener to update the feature flag when toggled
     toggle.addEventListener('change', () => {
         isFeatureEnabled = toggle.checked;
-        console.log('isFeatureEnabled: ', isFeatureEnabled)
 
         // Enable/Disable your feature
         if (isFeatureEnabled) {
             enableYourFeature();
+            localStorage.setItem('featureEnabled','true')
         } else {
             disableYourFeature();
+            localStorage.setItem('featureEnabled','false')
         }
 
         // Send the update to the backend
@@ -49,7 +51,7 @@ fetch(`/get_status/?project_url=${projectId}`)
         data.append('project_url', projectId);
         data.append('enable_feature', isFeatureEnabled.toString());
 
-        fetch('/enable_feature/', {
+        fetch('http://127.0.0.1:8000/enable_feature/', {
             method: 'POST',
             body: data,
         })
